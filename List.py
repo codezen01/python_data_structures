74c05926581dbd64fbdc5bb11db05732d3e0ab76
codezen <33179658+codezen01@users.noreply.github.com>
2022-02-27 18:32:36 +0530

    Update List.py

--- List.py
+++ List.py
@@ -16,3 +16,22 @@ concatenate	O(k)
 sort	O(n log n)
 multiply	O(nk)
 
+#===============================
+
+
+popzero = timeit.Timer("x.pop(0)",
+                       "from __main__ import x")
+popend = timeit.Timer("x.pop()",
+                      "from __main__ import x")
+
+x = list(range(2000000))
+popzero.timeit(number=1000)
+4.8213560581207275
+
+x = list(range(2000000))
+popend.timeit(number=1000)
+
+
+
+#==========================================
+
