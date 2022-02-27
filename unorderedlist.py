6192a945a1524e6165a7d8a54a267a023b004f64
codezen <33179658+codezen01@users.noreply.github.com>
2022-02-27 19:47:04 +0530

    Update unorderedlist.py

--- unorderedlist.py
+++ unorderedlist.py
@@ -50,6 +50,30 @@ class UnorderedList:
             current = current.getNext()
 
         return count
-    
+    def search(self,item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+
+        return found
+    def remove(self,item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+
+         if previous == None:
+            self.head = current.getNext()
+         else:
+            previous.setNext(current.getNext())
         
  mylist = UnorderedList()
