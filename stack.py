b33a5ee4a68df77656da9f2542cabfcda1a2dc73
codezen <33179658+codezen01@users.noreply.github.com>
2022-02-27 18:54:08 +0530

    Update stack.py

--- stack.py
+++ stack.py
@@ -1,1 +1,112 @@
 For example, every web browser has a Back button. As you navigate from web page to web page, those pages are placed on a stack (actually it is the URLs that are going on the stack).
+Stacks are ordered LIFO. The stack operations are given below.
+
+Stack() creates a new stack that is empty. It needs no parameters and returns an empty stack.
+push(item) adds a new item to the top of the stack. It needs the item and returns nothing.
+pop() removes the top item from the stack. It needs no parameters and returns the item. The stack is modified.
+peek() returns the top item from the stack but does not remove it. It needs no parameters. The stack is not modified.
+is_empty() tests to see whether the stack is empty. It needs no parameters and returns a boolean value.
+size() returns the number of items on the stack. It needs no parameters and returns an integer.
+For example, if s is a stack that has been created and starts out empty, then Table 1 shows the results of a sequence of stack operations. Under stack contents, the top item is listed at the far right.
+
+Stack Operation	Stack Contents	Return Value
+s.is_empty()	[]	True
+s.push(4)	[4]	 
+s.push('dog')	[4, 'dog']	 
+s.peek()	[4, 'dog']	'dog'
+s.push(True)	[4, 'dog', True]	 
+s.size()	[4, 'dog', True]	3
+s.is_empty()	[4, 'dog', True]	False
+s.push(8.4)	[4, 'dog', True, 8.4]	 
+s.pop()	[4, 'dog', True]	8.4
+s.pop()	[4, 'dog']	True
+s.size()	[4, 'dog']	2
+
+
+class Stack:
+     def __init__(self):
+         self.items = []
+
+     def is_empty(self):
+         return self.items == []
+
+     def push(self, item):
+         self.items.append(item)
+
+     def pop(self):
+         return self.items.pop()
+
+     def peek(self):
+         return self.items[len(self.items)-1]
+
+     def size(self):
+         return len(self.items)
+      
+
+def parChecker(symbolString):
+    s = Stack()
+    balanced = True
+    index = 0
+    while index < len(symbolString) and balanced:
+        symbol = symbolString[index]
+        if symbol in "([{":
+            s.push(symbol)
+        else:
+            if s.isEmpty():
+                balanced = False
+            else:
+                top = s.pop()
+                if not matches(top,symbol):
+                       balanced = False
+        index = index + 1
+    if balanced and s.isEmpty():
+        return True
+    else:
+        return False
+
+def matches(open,close):
+    opens = "([{"
+    closers = ")]}"
+    return opens.index(open) == closers.index(close)
+
+
+print(parChecker('{{([][])}()}'))
+print(parChecker('[{()]'))
+
+
+def divideBy2(decNumber):
+    remstack = Stack()
+
+    while decNumber > 0:
+        rem = decNumber % 2
+        remstack.push(rem)
+        decNumber = decNumber // 2
+
+    binString = ""
+    while not remstack.isEmpty():
+        binString = binString + str(remstack.pop())
+
+    return binString
+
+print(divideBy2(42))
+
+
+def baseConverter(decNumber,base):
+    digits = "0123456789ABCDEF"
+
+    remstack = Stack()
+
+    while decNumber > 0:
+        rem = decNumber % base
+        remstack.push(rem)
+        decNumber = decNumber // base
+
+    newString = ""
+    while not remstack.isEmpty():
+        newString = newString + digits[remstack.pop()]
+
+    return newString
+
+print(baseConverter(25,2))
+print(baseConverter(25,16))
+
